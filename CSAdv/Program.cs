ccbd68414a3f2ca81d5f9468eef326214943df10
thdud__ <sylim1114y@gmail.com>
2024-08-13 10:18:09 +0900

    output 키워드 실습 2

--- CSAdv/Program.cs
+++ CSAdv/Program.cs
@@ -49,6 +49,11 @@ namespace CSAdv
 
     internal class Program
     {
+        static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
+        }
         static void Main(string[] args)
         {
             Wanted<int> wantedInt = new Wanted<int>(65535);
@@ -73,6 +78,14 @@ namespace CSAdv
                 Console.WriteLine("숫자를 입력해주세요");
             }
 
+            int x = 0;
+            int y = 0;
+            int vx = 1;
+            int vy = 1;
+
+            Console.WriteLine("현재좌표  x : {0}, y : {1}", x, y);
+            NextPos(x, y, vx, vy, out x, out y);
+            Console.WriteLine("다음좌표  x : {0}, y : {1}", x, y);
         }
     }
 }
