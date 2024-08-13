0a179f9f94daf8456509909f6e2f94865879f350
thdud__ <sylim1114y@gmail.com>
2024-08-13 09:50:58 +0900

    제네릭 실습 1

--- CSAdv/Program.cs
+++ CSAdv/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace CSAdv
 {
+    class Wanted<T>
+    {
+        public T Value;
+        public Wanted(T value) {
+            Value = value;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Wanted<int> wantedInt = new Wanted<int>(65535);
+            Wanted<string> wantedString = new Wanted<string>("Hello, World");
+            Wanted<double> wantedDouble = new Wanted<double>(3.141592);
+
+            Console.WriteLine(wantedInt.Value);
+            Console.WriteLine(wantedString.Value);
+            Console.WriteLine(wantedDouble.Value);
         }
     }
 }
