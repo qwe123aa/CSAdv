fff75bb9f0dd3d7a937d028e49428bcd421f3a76
thdud__ <sylim1114y@gmail.com>
2024-08-13 10:17:30 +0900

    output 키워드 실습 1

--- CSAdv/Program.cs
+++ CSAdv/Program.cs
@@ -61,6 +61,18 @@ namespace CSAdv
 
             SquareCalculator s = new SquareCalculator();
             Console.WriteLine(s[256]);
+
+            Console.Write("숫자 입력 : ");
+            int output;
+            bool result = int.TryParse(Console.ReadLine(), out output);
+            if (result)
+            {
+                Console.WriteLine("입력한 숫자 : " + output);
+            }else
+            {
+                Console.WriteLine("숫자를 입력해주세요");
+            }
+
         }
     }
 }
